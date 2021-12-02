08a106890b72ee5556ba504b9aac430afc6c6542
lvshclt <lvshclt@gmail.com>
2021-12-02 17:06:31 +0900

    App.js 개괄적인 짜임새 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import GNB from './components/GNB';
+import TabBar from './components/TabBar';
+import Content from './components/Content';
+import AddGoals from './components/AddGoals';
+import Sorry from './components/Sorry';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="App container">
+        <div className="row">
+          <GNB />
+          <TabBar />
+          <Routes>
+            <Route path="/" element={<Content />} />
+            <Route path="/add-goals" element={<AddGoals />} />
+            <Route path="/sorry" element={<Sorry />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
