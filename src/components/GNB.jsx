a7e409452cf1c4b00e46de656ee9600209031dde
lvshclt <lvshclt@gmail.com>
2021-12-02 22:10:05 +0900

    GNB 현재 페이지 색깔로 나타내기 구현

--- src/components/GNB.jsx
+++ src/components/GNB.jsx
@@ -1,34 +1,46 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function GNB() {
+  const [load, setLoad] = useState(1);
+
+  function onClick(e) {
+    if (document.getElementsByClassName('on')[0])
+      [...document.getElementsByClassName('on')][0].classList.remove('on');
+    e.target.classList.add('on');
+    setLoad(load + 1);
+  }
+
   return (
-    <div className="gnb col-lg-12">
-      <div className="left col-lg-3">
-        <Link to="/">
-          <i className="ic-my-bucket-list yes"></i>
-        </Link>
+    load && (
+      <div className="gnb col-lg-12">
+        <div className="left col-lg-3">
+          <Link to="/">
+            <i className="ic-my-bucket-list yes"></i>
+          </Link>
+        </div>
+        <div className="right">
+          <Link to="/sorry">
+            <i className={`ic-search not-really`} onClick={onClick}></i>
+          </Link>
+
+          <Link to="/add-goals">
+            <i className="ic-plus-circle yes" onClick={onClick}></i>
+          </Link>
+
+          <Link to="/delete-goals">
+            <i className="ic-trash no" onClick={onClick}></i>
+          </Link>
+
+          <Link to="/settings">
+            <i className="ic-settings yes" onClick={onClick}></i>
+          </Link>
+
+          <Link to="/sorry ">
+            <i className="ic-sun not-really" onClick={onClick}></i>
+          </Link>
+        </div>
       </div>
-      <div className="right">
-        <Link to="/sorry">
-          <i className="ic-search not-really"></i>
-        </Link>
-
-        <Link to="/add-goals">
-          <i className="ic-plus-circle yes"></i>
-        </Link>
-
-        <Link to="/delete-goals">
-          <i className="ic-trash no"></i>
-        </Link>
-
-        <Link to="/settings">
-          <i className="ic-settings yes"></i>
-        </Link>
-
-        <Link to="/sorry ">
-          <i className="ic-sun not-really"></i>
-        </Link>
-      </div>
-    </div>
+    )
   );
 }
